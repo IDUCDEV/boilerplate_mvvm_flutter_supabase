bedb8905ee6a99ed4ec8e68633033e1bac39754f
IducDev <urdanetacuarteisaacdavid@gmail.com>
2025-11-11 12:42:38 -0400

    splash screen ready

--- lib/src/views/splash_screen/splash_screen.dart
+++ lib/src/views/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:go_router/go_router.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import '../../colors/app_theme.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -10,24 +11,49 @@ class SplashView extends StatefulWidget {
 }
 
 class _SplashViewState extends State<SplashView> {
+  double progressValue = 0.0;
+
   @override
   void initState() {
     super.initState();
-    _checkSession();
-  }
 
-  Future<void> _checkSession() async {
-    final prefs = await SharedPreferences.getInstance();
-    final logged = prefs.getBool('logged_in') ?? false;
-    await Future.delayed(const Duration(seconds: 2));
-    if (!mounted) return;
-    context.go(logged ? '/home' : '/login');
+    Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+      setState(() {
+        progressValue += 0.3333; // Increment progress by 33.33% every second
+        if (progressValue >= 1.0) {
+          timer.cancel();
+          // Navigate to login, redirect will handle based on auth
+          GoRouter.of(context).go('/login');
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return Scaffold(
+      backgroundColor: AppTheme.lightTheme.scaffoldBackgroundColor,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(17.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Replace with your app logo
+              Text("Splash Screen"),
+              const SizedBox(height: 16),
+              const Text(
+                '© 2025 Developed By IDUCDEV',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
